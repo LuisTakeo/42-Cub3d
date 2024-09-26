61b889bbc5f07f9c1feaaa118cbb31a968df07f1
LuisTakeo <luistakeo@hotmail.com>
2024-09-26 16:40:49 -0300

    feat: movements

--- src/init_game.c
+++ src/init_game.c
@@ -12,6 +12,21 @@
 
 #include "../includes/cub3d.h"
 
+void	listen_moves(mlx_key_data_t keydata, void *param)
+{
+	t_cub3d	*cub3d;
+
+	cub3d = (t_cub3d *)param;
+	if (keydata.key == MLX_KEY_W)
+		printf("Walking Front\n");
+	if (keydata.key == MLX_KEY_S)
+		printf("Walking Back\n");
+	if (keydata.key == MLX_KEY_A)
+		printf("Walking Left\n");
+	if (keydata.key == MLX_KEY_D)
+		printf("Walking Right\n");
+}
+
 void	init_values(t_cub3d *cub3d)
 {
 	cub3d->mlx = NULL;
@@ -23,6 +38,7 @@ int	init_game(t_cub3d *cub3d)
 	cub3d->mlx = mlx_init(1280, 800, "Cub3d", true);
 	if (!cub3d->mlx)
 		return (EXIT_FAILURE);
+	mlx_key_hook(cub3d->mlx, &listen_moves, cub3d);
 	mlx_loop(cub3d->mlx);
 	return (EXIT_SUCCESS);
 }
